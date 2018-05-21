5b843bcbb757b8164e96ed9299b628e5f9a170bc
Jieyi <pokkbaby@gmail.com>
2018-05-21 19:01:38 +0900

    Added the rx core data operations.

--- Shrubbery/DataManager/Local/ShrubberyCoreData.swift
+++ Shrubbery/DataManager/Local/ShrubberyCoreData.swift
@@ -6,10 +6,32 @@
 //  Copyright © 2018 CloverLab. Inc. All rights reserved.
 //
 
+import CoreData
 import RxSwift
+import RxCoreData
+import ObjectMapper
 
 class ShrubberyCoreData: LocalCoreData {
+    @available(iOS 10.0, *)
+    lazy var coreDataContext: NSManagedObjectContext! = {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }()
+
+    @available(iOS 10.0, *)
     func retrieveFakeList() -> Single<FakeEntity> {
-        fatalError("retrieveFakeList() has not been implemented")
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Fake")
+        do {
+            try coreDataContext.rx.update(InformationEntity(id: 32, title: "vvvvvvvvvvvv", updatedAt: Date()))
+        }
+        catch {
+        }
+
+        coreDataContext.rx.entities(InformationEntity.self, sortDescriptors: [NSSortDescriptor(key: "updated_at", ascending: false)])
+            .subscribe { event in
+                logw(event)
+            }
+
+        return Single.just(FakeEntity(map: Map(mappingType: .toJSON, JSON: ["123": "123"]))!, scheduler: MainScheduler())
     }
 }
--- Shrubbery/Entities/InformationEntity+Ext.swift
+++ Shrubbery/Entities/InformationEntity+Ext.swift
@@ -7,6 +7,8 @@
 //
 
 import ObjectMapper
+import RxCoreData
+import CoreData
 
 struct InformationEntity {
     var id: Int?
@@ -22,8 +24,41 @@ extension InformationEntity: Mappable {
     }
 
     mutating func mapping(map: Map) {
-        id <- map["map"]
+        id <- map["id"]
         title <- map["title"]
         updatedAt <- (map["updated_at"], DateTransform())
+    }
+}
+
+extension InformationEntity: Persistable {
+    typealias T = NSManagedObject
+
+    static var entityName: String {
+        return "Fake"
+    }
+    static var primaryAttributeName: String {
+        return "id"
+    }
+    public var identity: String {
+        return "this is cool"
+    }
+
+    public init(entity: T) {
+        id = entity.value(forKey: "id") as! Int
+        title = entity.value(forKey: "title") as! String
+        updatedAt = entity.value(forKey: "updated_at") as! Date
+    }
+
+    public func update(_ entity: T) {
+        entity.setValue(id, forKey: "id")
+        entity.setValue(title, forKey: "title")
+        entity.setValue(updatedAt, forKey: "updated_at")
+
+        do {
+            try entity.managedObjectContext?.save()
+        }
+        catch let e {
+            loge(e)
+        }
     }
 }
